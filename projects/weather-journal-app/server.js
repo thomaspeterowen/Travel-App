8b0cea84313be6dca8ff6a5c4bc58f7ba91d70a0
Thomas Owen <thomaspeterowen@gmail.com>
2023-01-13 16:09:55 +0100

    complete project ready for Udacity submit

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,19 +1,42 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = [];
 
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
 
+const bodyParser = require("body-parser");
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
+// GET route
+app.get("/all", (req, res) => {
+  res.send(projectData);
+});
+
+// POST an entry route
+app.post("/addEntry", addEntry);
+
+function addEntry(req, res) {
+  projectData.push(req.body);
+}
 
 // Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+
+function listening() {
+  console.log("server running");
+  console.log(`running on localhost: ${port}`);
+}
